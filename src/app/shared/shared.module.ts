7a68e75c955b08c8e66469e12b6a80683b2fea43
Facundo <wehrlifacundo9@gmail.com>
2023-07-18 11:33:41 -0300

    add(home, about): basic routing

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [AboutPageComponent,
+                 HomePageComponent,
+                 SidebarComponent],
+  imports: [CommonModule,
+            RouterModule],
+  exports: [AboutPageComponent,
+            HomePageComponent,
+            SidebarComponent,],
+})
+
+export class SharedModule {}
