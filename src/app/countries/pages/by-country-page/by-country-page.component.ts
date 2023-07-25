2460203bc16adf561a91a70dc3509734bff11038
Facundo <wehrlifacundo9@gmail.com>
2023-07-25 10:20:56 -0300

    feat: search by country

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/contries.service';
+import { Country } from '../../interfaces/contry';
 
 @Component({
   selector: 'app-by-country-page',
   templateUrl: './by-country-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ByCountryPageComponent {
+  constructor(private CountriesServiceesService: CountriesService) {}
+  public countries: Country[] = [];
 
+  searchByCountry(term: string): void {
+    this.CountriesServiceesService.searchCountry(term).subscribe(
+      (countries) => {
+        this.countries = countries;
+      }
+    );
+  }
 }
